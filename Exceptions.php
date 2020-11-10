44012004de310772d41d990e1876e87cb4c7d68a
codeBull-camelCaseOnly <58913047+codeBull-camelCaseOnly@users.noreply.github.com>
2020-11-10 11:42:01 +0530

    Added Database class, Exception class, composer files

---
+++ Exceptions.php
@@ -0,0 +1,5 @@
+<?php
+
+class ConnectionException extends Exception
+{
+}
--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-$hi = 'HI THERE';
+require __DIR__ . '/vendor/autoload.php';
+require_once('Database.php');
 
-echo $hi;
+$dotEnv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotEnv->load();
 
+echo 'testing database connection class...';
+
+try {
+    $database = new Database($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
+} catch (ConnectionException $e) {
+    echo "FAILED!";
+}
+
+echo "Prob success...";
